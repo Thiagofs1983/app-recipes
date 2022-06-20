9a334ca245632115a48ae9c7f151cdab12bfad3f
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-20 17:15:36 -0300

    criado componente Input

---
+++ src/components/Login/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Input(type, name, value, handleChange) {
+  return (
+    <input
+      type={ type }
+      name={ name }
+      value={ value }
+      onChange={ handleChange }
+    />
+  );
+}
+
+export default Input;
