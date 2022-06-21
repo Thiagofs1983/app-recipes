2392e7c928d32947c3dfa2a10113f152a1510d53
Priscila Nogueira <priscilanogueiraferreira27@gmail.com>
2022-06-21 18:35:42 -0300

    Atualizando arquivo para requisitos 10 e 11

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,30 +9,39 @@ function Header({ namePage, isEnable }) {
 
   return (
     <header>
-      <Link to="/profile">
-        <input
-          type="image"
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-          alt="profile button"
-        />
-      </Link>
+      <div>
+        <Link to="/profile">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="profile button"
+          />
+        </Link>
 
-      <h1 data-testid="page-title">{ namePage }</h1>
+        <h1 data-testid="page-title">{ namePage }</h1>
 
-      {isEnable && (
+        { isEnable && (
+          <button
+            type="button"
+            onClick={ () => setSearchBar(!searchBar) }
+          >
+            <img
+              src={ searchIcon }
+              alt="search button"
+              data-testid="search-top-btn"
+            />
+          </button>
+        ) }
+      </div>
+
+      { searchBar && (
         <div>
           <input
-            type="image"
-            data-testid="search-top-btn"
-            src={ searchIcon }
-            alt="search button"
-            onClick={ () => setSearchBar(!searchBar) }
+            type="text"
+            data-testid="search-input"
           />
-
-          { searchBar && <p data-testid="search-input">Mostrar Barra</p> }
         </div>
-      )}
+      ) }
 
     </header>
   );
