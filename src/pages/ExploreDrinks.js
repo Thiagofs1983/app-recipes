95cdde0d10c234bdf3b006e6a91b11bc6cf69f79
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-21 13:29:41 -0300

    redireciona para a pagina drinks igredients ao clicar no botão

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 import ButtonExplore from '../components/Explore/Button';
 
 function ExploreDrinks() {
+  const history = useHistory();
+
+  const clickExploreDrinksIgredient = () => {
+    history.push('/explore/drinks/ingredients');
+  };
+
   return (
     <div>
       <ButtonExplore
         testId="explore-by-ingredient"
         buttonText="By Ingredient"
+        handleClick={ clickExploreDrinksIgredient }
       />
       <ButtonExplore
         testId="explore-surprise"
