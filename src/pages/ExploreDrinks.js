bbdaae8870c89edd8a266babff6ce505f9ad1157
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-21 13:00:36 -0300

    criada a pagina de explore drinks

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
+import ButtonExplore from '../components/Explore/Button';
 
 function ExploreDrinks() {
   return (
     <div>
-      ExploreDrinks
+      <ButtonExplore
+        testId="explore-by-ingredient"
+        buttonText="By Ingredient"
+      />
+      <ButtonExplore
+        testId="explore-surprise"
+        buttonText="Surprise me!"
+      />
       <Footer />
     </div>
   );
