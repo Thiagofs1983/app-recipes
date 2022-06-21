fda11659124e46dde2e4ed65a8ad3d1ce235a26e
devpbdias <devpbdias@gmail.com>
2022-06-21 10:22:09 -0300

    Fiz a busca da chave email do localStorage e criei os botoes redirecionadores, limpei o storage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 
 function Profile() {
+  const userProfile = localStorage.getItem('user');
+  const userEmail = JSON.parse(userProfile);
+
   return (
-    <div>
-      Profile
+    <section>
+      <h1>Profile</h1>
+      <p data-testid="profile-email">{ userEmail.email }</p>
+      <div>
+        <Link to="/done-recipes">
+          <button
+            data-testid="profile-done-btn"
+            type="button"
+          >
+            Done Recipes
+          </button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+            onClick={ () => localStorage.clear() }
+          >
+            Logout
+          </button>
+        </Link>
+      </div>
       <Footer />
-    </div>
+    </section>
   );
 }
 
