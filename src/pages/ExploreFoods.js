3034b591a55cf95c3cd1e2bc1d55f004c698ca80
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-21 12:57:07 -0300

    criada a pagina de explore foods

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
+import ButtonExplore from '../components/Explore/Button';
 
 function ExploreFoods() {
   return (
     <div>
-      ExploreFoods
+      <ButtonExplore
+        testId="explore-by-ingredient"
+        buttonText="By Ingredient"
+      />
+      <ButtonExplore
+        testId="explore-by-nationality"
+        buttonText="By Nationality"
+      />
+      <ButtonExplore
+        testId="explore-surprise"
+        buttonText="Surprise me!"
+      />
       <Footer />
     </div>
   );
