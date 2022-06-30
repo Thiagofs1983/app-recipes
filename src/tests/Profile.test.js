e5b647e251fabc1cfc350ee55d161ba738c57e18
Jonathan Santos <jonathan01347@gmail.com>
2022-06-30 15:14:09 -0300

    ajustes iniciais nos tests

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from '../App';
+// import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import mockExploreDrinks from '../mock/mockExploreDrinks';
 import ingredientesDrinks from '../mock/mockIngredientsDrinks';
