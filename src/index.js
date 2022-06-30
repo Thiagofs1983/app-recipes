0a22b747936f47c6b1b56a0c9409dd378ab77cac
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-30 15:47:21 -0300

    trocando as importações dos providers do index para o app.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import FoodDrinkProvider from './context/FoodDrink/FoodDrinkProvider';
-import ProductDetailsProvider from './context/FoodDetails/ProductDetailsProvider';
 
 ReactDOM.render(
   <BrowserRouter>
-    <FoodDrinkProvider>
-      <ProductDetailsProvider>
-        <App />
-      </ProductDetailsProvider>
-    </FoodDrinkProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
